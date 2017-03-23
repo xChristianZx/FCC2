87fc89658a64376a1b8dac0b94986b3a2306ff72
Christian Zenaty <cazenaty@yahoo.com>
2017-03-22 22:30:43 -0700

    added axios

--- Projects/wiki_viewer/app.js
+++ Projects/wiki_viewer/app.js
@@ -1,2 +1,25 @@
 // 1. Can search Wikipedia entries in a search box and see a list of results
 // 2. Can click a button to see a random wikipedia entry
+
+var root = "https://randomuser.me/api/";
+
+/*
+fetch(root)
+  .then(function (response) {
+    console.log(response.json());
+    console.log('Success:');
+    console.log(response);
+  })
+  .catch(function (err) {
+    console.error('error: ', err);
+  });
+*/
+
+axios.get(root)
+  .then(function (response) {
+    console.log('Axios Success!');
+    console.log(response);
+  })
+  .catch(function(err){
+    console.log(err);
+  });
