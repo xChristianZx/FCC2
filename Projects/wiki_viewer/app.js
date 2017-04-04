380e0682d7528bb597c3a4689475d356d33d1afc
Christian Zenaty <cazenaty@yahoo.com>
2017-04-04 12:28:30 -0700

    tiff fixed keypress()

--- Projects/wiki_viewer/app.js
+++ Projects/wiki_viewer/app.js
@@ -8,13 +8,16 @@
 
 $(document).ready(function(){
 
-    //Click listener
-    $('#search-button').click(runSearch);
+  //Click listener
+  $('#search-button').click(runSearch);
 
-    //keypress listener
-    $('#search-form').keypress(function(event){
-      if (event.which == 13) {$('#search-button').click();}
-    });
+  //keypress listener
+  $('#search-form').keypress(function(event){
+    if (event.which == 13) {
+      event.preventDefault();
+      runSearch();
+    }
+  });
 
   function runSearch() {
     var searchItem = $('#search-form').val();
@@ -26,17 +29,10 @@ $(document).ready(function(){
     const wikiTwo = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchItem + '&format=json&origin=*';
 
     function renderHTML(response) {
+      $('.results-list').html('');
       for (var i = 0; i < response.data[1].length; i++){
         $('.results-list').append('<div><a href="' + response.data[3][i] + '" target = "_blank"><li><h5>' + response.data[1][i] + '</h5><p>' + response.data[2][i] + '</p></li></a></div>');
       }
-    }
-//trying to use forEach - arggghhhhhhh!
-    function renderHTML2(response) {
-      var data = response.data;
-      data.forEach(data, function (element, index, array) {
-        //$('.results-list').append('<li><p>' + element[2][index] + '</p></li>');
-        console.log('I AM HERE: ', data[1] );
-      });
     }
 /*
     fetch(wikiTwo)
@@ -60,18 +56,10 @@ $(document).ready(function(){
       });
 */
 
-var json;
-var dataNames, dataDescription, dataUrl;
-
     axios.get(wikiTwo)
       .then(function (response) {
         console.log('Axios Success!');
         console.log('Axios Response: ', response);
-
-        json = JSON.stringify(response.data);
-        dataNames = response.data[1];
-        dataDescription = response.data[2];
-        dataUrl = response.data[3];
 
         //json.forEach(function(e){console.log(e);})
         console.log(response.data);
@@ -81,12 +69,11 @@ var dataNames, dataDescription, dataUrl;
         console.log(response.data[1][0]);
         console.log(response.data[2][0]);
         console.log(response.data[3][0]);
-        $('.results-list').html('')
         renderHTML(response);
       })
       //.then(function(){console.log('TESTING:', json);})
       .catch(function(err){
-        console.log('Error: ', err);
+        console.log('Axios Error: ', err);
       });
 
   }
