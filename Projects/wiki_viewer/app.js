05b9db30b25cc074d5b729371eb389176e76e48d
Christian Zenaty <cazenaty@yahoo.com>
2017-04-03 23:32:44 -0700

    working results field

--- Projects/wiki_viewer/app.js
+++ Projects/wiki_viewer/app.js
@@ -12,16 +12,12 @@ $(document).ready(function(){
     $('#search-button').click(runSearch);
 
     //keypress listener
-    $('#search-form').keypress(function(){
-      if (event.which == 13) runSearch();
+    $('#search-form').keypress(function(event){
+      if (event.which == 13) {return runSearch();}
     });
 
   function runSearch() {
     var searchItem = $('#search-form').val();
-    //document.querySelector('#search-form').value;
-
-    //Random User test API
-    //const root = "https://randomuser.me/api/";
 
     //using query
     const wiki = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json";
@@ -30,12 +26,25 @@ $(document).ready(function(){
     const wikiTwo = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchItem + '&format=json&origin=*';
 
     function renderHTML(response) {
-      $('.results-list').append('<li>' + response.data[0] + '</li>');
-      $('.results-list').append('<li>' + response.data[1][0] + '</li>');
-      $('.results-list').append('<li>' + response.data[2][0] + '</li>');
-      $('.results-list').append('<li>' + response.data[3][0] + '</li>');
+    /*
+      $('.results-list').append('<li><h4>Search results for: "' + response.data[0] + '"</h4></li>');
+      $('.results-list').append('<li><h5>' + response.data[1][0] + '</h5></li>');
+      $('.results-list').append('<li><p>' + response.data[2][0] + '</p></li>');
+      $('.results-list').append('<li><a href="' + response.data[3][0] + '" target = "_blank">' + response.data[3][0] + '</a></li>');
+      $('.results-list').append('<div><li><h5>' + response.data[1][0] + '</h5><p>' + response.data[2][0] + '</p></li></div>');
+    */
+      for (var i = 0; i < response.data[1].length; i++){
+        $('.results-list').append('<div><a href="' + response.data[3][i] + '" target = "_blank"><li><h5>' + response.data[1][i] + '</h5><p>' + response.data[2][i] + '</p></li></a></div>');
+      }
     }
 
+    function renderHTML2(response) {
+      var data = response.data;
+      data.forEach(data, function (element, index, array) {
+        //$('.results-list').append('<li><p>' + element[2][index] + '</p></li>');
+        console.log('I AM HERE: ', data[1] );
+      });
+    }
 /*
     fetch(wikiTwo)
       .then(function (response){
@@ -59,13 +68,21 @@ $(document).ready(function(){
 */
 
 var json;
+var dataNames, dataDescription, dataUrl;
+
     axios.get(wikiTwo)
       .then(function (response) {
         console.log('Axios Success!');
         console.log('Axios Response: ', response);
 
         json = JSON.stringify(response.data);
+        dataNames = response.data[1];
+        dataDescription = response.data[2];
+        dataUrl = response.data[3];
+
+        //json.forEach(function(e){console.log(e);})
         console.log(response.data);
+
         //console.log("AXIOS JSON:", json);
         console.log(response.data[0]);
         console.log(response.data[1][0]);
@@ -77,7 +94,6 @@ var json;
       .catch(function(err){
         console.log(err);
       });
-
 
   }
 
