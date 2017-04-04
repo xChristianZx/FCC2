02dc1f3383d9643c56e0818004df7a7961806123
Christian Zenaty <cazenaty@yahoo.com>
2017-04-04 00:05:49 -0700

    update

--- Projects/wiki_viewer/app.js
+++ Projects/wiki_viewer/app.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
     //keypress listener
     $('#search-form').keypress(function(event){
-      if (event.which == 13) {return runSearch();}
+      if (event.which == 13) {$('#search-button').click();}
     });
 
   function runSearch() {
@@ -26,18 +26,11 @@ $(document).ready(function(){
     const wikiTwo = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchItem + '&format=json&origin=*';
 
     function renderHTML(response) {
-    /*
-      $('.results-list').append('<li><h4>Search results for: "' + response.data[0] + '"</h4></li>');
-      $('.results-list').append('<li><h5>' + response.data[1][0] + '</h5></li>');
-      $('.results-list').append('<li><p>' + response.data[2][0] + '</p></li>');
-      $('.results-list').append('<li><a href="' + response.data[3][0] + '" target = "_blank">' + response.data[3][0] + '</a></li>');
-      $('.results-list').append('<div><li><h5>' + response.data[1][0] + '</h5><p>' + response.data[2][0] + '</p></li></div>');
-    */
       for (var i = 0; i < response.data[1].length; i++){
         $('.results-list').append('<div><a href="' + response.data[3][i] + '" target = "_blank"><li><h5>' + response.data[1][i] + '</h5><p>' + response.data[2][i] + '</p></li></a></div>');
       }
     }
-
+//trying to use forEach - arggghhhhhhh!
     function renderHTML2(response) {
       var data = response.data;
       data.forEach(data, function (element, index, array) {
@@ -88,11 +81,12 @@ var dataNames, dataDescription, dataUrl;
         console.log(response.data[1][0]);
         console.log(response.data[2][0]);
         console.log(response.data[3][0]);
+        $('.results-list').html('')
         renderHTML(response);
       })
       //.then(function(){console.log('TESTING:', json);})
       .catch(function(err){
-        console.log(err);
+        console.log('Error: ', err);
       });
 
   }
