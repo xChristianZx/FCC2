64bd9ac0672a4f0e1dc730a6cb1c07126394a39b
Christian Zenaty <cazenaty@yahoo.com>
2017-04-11 22:36:42 -0700

    general layout

--- Projects/twitch_tv/app.js
+++ Projects/twitch_tv/app.js
@@ -7,34 +7,39 @@
 // User Story: I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.
 
 $(document).ready(function () {
-  
-  let twitch = 'https://api.twitch.tv/kraken'
-  let url = 'https://wind-bow.gomix.me/twitch-api';
-  let targetUrl = '/streams/freecodecamp';
-  let user = '/users/valkia'
-  
-  let test = url + user;
+
+    let twitch = 'https://api.twitch.tv/kraken';
+    let url = 'https://wind-bow.gomix.me/twitch-api';
+    let free = '/streams/freecodecamp';
+    let user = '/users/valkia';
+    let user2 = '/users/ESL_SC2';
+
+    let test = url + user2;
 
     axios.get(test)
-    .then(function(response){
-        console.log(response);
-        console.log('Stream: ', response.data.stream)
-        streamCheck(response);
-    })
-    .catch(function (err){
-        console.log('Error: ', err);
-    });
+        .then(function (response) {
+            console.log(response);
+            console.log('Stream: ', response.data.stream);
+            console.log('Name: ', response.data.display_name);
+            console.log('Link: ', response.data._links.self);
+            streamCheck(response);
+        })
+        .catch(function (err) {
+            console.log('Error: ', err);
+        });
 
-    function streamCheck (resp) { 
-        if (resp.data.stream == null){
-            console.log('Live stream is not on');
-        } else if (resp.data.stream == undefined){
+    function streamCheck(resp) {
+        if (resp.data.stream === null) {
+            console.log('Not currently streaming');
+        } else if (!resp.data.stream) {
             console.log('Streaming');
         } else {
             console.log('Hmmmmm');
         }
+    };
 
-        
-     };
+    function renderHTML(response) {
+        $('.box-1').append()
+    }
 
 });
