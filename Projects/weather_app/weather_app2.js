5ff74be7a8f5890c93fc2142ef8c7fa41be37eef
Christian Zenaty <cazenaty@yahoo.com>
2017-05-02 15:22:12 -0700

    added date

--- Projects/weather_app/weather_app2.js
+++ Projects/weather_app/weather_app2.js
@@ -2,32 +2,32 @@
 // 2. different icon or background image for the type of weather
 // 3. toggle between Fahrenheit and Celsius
 
-$(document).ready(function(){
-//API Call to get user location
+$(document).ready(function () {
+  //API Call to get user location
 
   const apiKey = "be12c9cede78dacab40103d7c20d67fb";
 
-  function getUserInfo(){
-  //Get user location via IP address
-    $.getJSON("http://ip-api.com/json", function(json){
+  function getUserInfo() {
+    //Get user location via IP address
+    $.getJSON("http://ip-api.com/json", function (json) {
       var currentLat = json.lat;
       var currentLong = json.lon;
       var city = json.city;
       var state = json.region;
 
       var apiCall = "http://api.openweathermap.org/data/2.5/weather?lat=" + currentLat + "&lon=" + currentLong + "&units=imperial" + "&APPID=be12c9cede78dacab40103d7c20d67fb";
-/*
-// fetch() testing
-      fetch(apiCall)
-      .then((response)=> {
-        console.log();
-        console.log("Fetch Status: ", response.status);
-        response.json().then((data) => {console.log('fetch: ',data.name);})
+      /*
+      // fetch() testing
+            fetch(apiCall)
+            .then((response)=> {
+              console.log();
+              console.log("Fetch Status: ", response.status);
+              response.json().then((data) => {console.log('fetch: ',data.name);})
 
-      })
-      .catch(err => {console.log(err);});
-*/
-  //API Call to Openweathermap
+            })
+            .catch(err => {console.log(err);});
+      */
+      //API Call to Openweathermap
       axios.get(apiCall)
         .then(function (response) {
           console.log('API Call: Success', response);
@@ -36,8 +36,7 @@ $(document).ready(function(){
         .catch(function (error) {
           console.log('API Call: Failed');
           console.log(error);
-
-      });
+        });
     });
   }
   getUserInfo();
@@ -45,57 +44,55 @@ $(document).ready(function(){
 
 
 //HTML Rendering
-function renderHTML (response){
+function renderHTML(response) {
 
   var currentTemp = response.data.main.temp;
   console.log("this is temp", currentTemp);
 
   var currentFar = farRound(currentTemp);
   var currentCels = celsConvert(currentTemp);
-    // Temp Conversions
-  function farRound(temp){
-      return Math.floor(temp);
-    }
+  // Temp Conversions
+  function farRound(temp) {
+    return Math.floor(temp);
+  }
 
-  function celsConvert (temp){
-      return Math.floor((temp - 32) * (5/9));
-    }
+  function celsConvert(temp) {
+    return Math.floor((temp - 32) * (5 / 9));
+  }
 
-  console.log("These are the degs", currentFar+'F', currentCels+'C');
+  console.log("These are the degs", currentFar + 'F', currentCels + 'C');
 
-//html rendering
+  //html rendering
   $('#city').html(response.data.name);
   $('#temp').html(currentFar + "&#176 F");
   $('#desc').html(response.data.weather[0].main);
   $('#icon-id').html(response.data.weather[0].id);
 
-//F/C unit toggle
-//EXPLANATION: As I click the button, it toggles between
-//adding and removing the class units-toggle-on.
-//as it does this, hasClass checks each time and switches between the two
+  //F/C unit toggle
+  //EXPLANATION: As I click the button, it toggles between
+  //adding and removing the class units-toggle-on.
+  //as it does this, hasClass checks each time and switches between the two
 
-  $('body').click(function(){
+  $('body').click(function () {
     $('.templine').toggleClass('units-toggle-on');
-    if($('.templine').hasClass('units-toggle-on')) {
+    if ($('.templine').hasClass('units-toggle-on')) {
       $('#temp').html(currentFar + "&#176 F");
     } else {
       $('#temp').html(currentCels + "&#176 C");
     }
   });
 
-
-getIcon(response);
-
+  getIcon(response);
 }
 
 // Weather Icon ID number
 
-function getIcon (response){
+function getIcon(response) {
   var icon;
   var currentIconNumber = response.data.weather[0].id;
   console.log('currentIconNumber: ', currentIconNumber);
 
-  function indexer (val){
+  function indexer(val) {
     if (val > 800) {
       switch (val) {
         case 800:
@@ -121,8 +118,8 @@ function getIcon (response){
           icon = 'wi-cloudy';
       }
     } else {
-    //Simplifies the icon number by the first character so that it is
-    //easier to display weather icons according to icon number
+      //Simplifies the icon number by the first character so that it is
+      //easier to display weather icons according to icon number
       var iconIndex = val.toString().charAt(0);
 
       console.log('TASTY says:', iconIndex);
@@ -149,11 +146,11 @@ function getIcon (response){
           icon = 'wi-fog';
           break;
         case '8':
-        //clear
-          icon  = 'wi-day-sunny';
+          //clear
+          icon = 'wi-day-sunny';
           break;
         default:
-        //Clouds for the default
+          //Clouds for the default
           icon = 'wi-cloudy';
           break;
       }
@@ -162,5 +159,19 @@ function getIcon (response){
   indexer(currentIconNumber);
 
   $('#weather-icon').html('<i class = "wi ' + icon + '"></i>');
-  console.log('CHECK THE ICON Variable', icon);
+  console.log('Icon Variable', icon);
 }
+
+!function getDate() {
+  var date = new Date();
+  var weekday = date.getDay();
+  var month = date.getMonth();
+  var day = date.getDate();
+
+  const monthsList = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov', 'Dec'];
+  const dayWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  $('#date').html(dayWeek[weekday] + ', ' + monthsList[month] + ' ' + day);
+
+  console.log(weekday, month, day);
+}();
