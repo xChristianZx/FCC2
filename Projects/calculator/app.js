ee4701e00fbac172fd6ef369c7615493ac10fa9e
Christian Zenaty <cazenaty@yahoo.com>
2017-06-05 22:40:19 -0700

    errant zero fixed

--- Projects/calculator/app.js
+++ Projects/calculator/app.js
@@ -1,63 +1,69 @@
 //TO DO
 // 1. joining history array without commas
-// 2. removing 0 from initial inputArr - use initialize
+// 2. fix after equality followed by more number instead of operator
+
+// post equality(true) =>
+//     1. number []
+//     2. operator []
+//     3. clear []
 
 $(document).ready(function () {
     const clearArr = [0];
     var inputArr = [];
     var historyArr = [];
+    var equality = false;
 
-    function initialize () {
+    function initialize() {
         inputArr = [];
         historyArr = [];
+        var equality = false;
         $('#output-text').text(clearArr[0]);
         $('#history-text').text(clearArr[0]);
     };
 
     //Print to form
-    function printToDisplay () {
+    function printToDisplay() {
         $('#output-text').text(inputArr.join(''));
         $('#history-text').text(historyArr.join(''));
     };
-    
-    function errantZero () {
+
+    function errantZero() {
         const regExp = /[0-9]/g;
         if (inputArr[0] == 0 && inputArr[1] == regExp) {
             inputArr.shift();
         };
     };
 
-    function compute () {
-      var test = inputArr.join('');
-      var equals = math.eval(test);
-      console.log('test', test, 'equals', equals)
-      //console.log(typeof test);
-      $('#output-text').text(equals);
-      historyArr.push(equals);
-      $('#history-text').text(inputArr);
-      inputArr = [];
-      inputArr.push(equals);
+    function compute() {
+        var test = inputArr.join('');
+        var equals = math.eval(test);
+        console.log('test', test, 'equals', equals)
+        //console.log(typeof test);
+        $('#output-text').text(equals);
+        historyArr.push(equals);
+        $('#history-text').text(inputArr);
+        inputArr = [];
+        inputArr.push(equals);
+        var equality = true;
+        console.log('equality', equality);
     };
 
     // AC all clear
     $('#all-clear').click(function () {
-        inputArr = [];
-        historyArr = [];
-        $('#output-text').text(clearArr[0]);
-        $('#history-text').text(clearArr[0]);
-        console.log('All Clear', inputArr);
+        initialize();
+        console.log('AC', inputArr, historyArr);
     });
 
     // CE clear entry - only clears input
-    $('#clear-entry').click(function (){
+    $('#clear-entry').click(function () {
         inputArr = [];
         $('#output-text').text(clearArr[0]);
         console.log('Clear Entry', inputArr);
-    }); 
+    });
 
-    // button click event
-    $('button').click(function (e) {
-        errantZero();
+    // number button click event
+    $('.number').click(function (e) {
+        //errantZero();
         var number = $(this).attr('value');
         $('#output-text').text(number);
         $('#history-text').text(number);
@@ -65,10 +71,20 @@ $(document).ready(function () {
         console.log(inputArr)
         //console.log(e);
         printToDisplay();
-    })
+    });
 
-     $('#equals').click(function () {
-        compute();        
+    // operator button click - in order to clear 0 from initial input
+    $('.operator').click(function (e) {
+        var number = $(this).attr('value');
+        $('#output-text').text(number);
+        $('#history-text').text(number);
+        inputArr.push(number)
+        console.log(inputArr)
+        //console.log(e);
+        printToDisplay();
+    });
+
+    $('#equals').click(function () {
+        compute();
     });
 });
-
