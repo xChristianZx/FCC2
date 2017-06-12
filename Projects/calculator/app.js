48d3727a3a5d283e50767e5c50f17e1e9e71006e
Christian Zenaty <cazenaty@yahoo.com>
2017-06-12 15:24:34 -0700

    fixed decimal button and summation bugs

--- Projects/calculator/app.js
+++ Projects/calculator/app.js
@@ -1,24 +1,37 @@
 //TO DO
 
-// 1. fix after equality followed by more number instead of operator
-
-// post equality(true) =>
-//     1. number [x]
-//     2. operator []
-//     3. clear [x]
+// fix history bug when doing multiple operators with equality 
+// plus/ minus function
+// idea: 3 arrays - input, holding, history = see iphone
 
 $(document).ready(function () {
     const clearArr = [0];
     var inputArr = [];
     var historyArr = [];
-    var equality = false;
+    var equality;
     var test;
     var equals;
+    var decimal;
+    initialize();
+
+    function equalsReset() {
+        equals = false;
+    };
+
+    function decimalReset() {
+        decimal = false;
+    };
+
+    function equalityReset() {
+        equality = false;
+    };
 
     function initialize() {
         inputArr = [];
         historyArr = [];
-        equality = false;
+        equalsReset();
+        decimalReset();
+        equalityReset();
         console.log('equality: ', equality);
         $('#output-text').text(clearArr[0]);
         $('#history-text').text(clearArr[0]);
@@ -30,37 +43,47 @@ $(document).ready(function () {
         $('#history-text').text(historyArr.join(''));
     };
 
-    function errantZero() {
-        const regExp = /[0-9]/g;
-        if (inputArr[0] == 0 && inputArr[1] == regExp) {
-            inputArr.shift();
-        };
-    };
-
     function numberInput() {
-        //refactor for below
-
-        // var number = $(this).attr('value');
-        //     $('#output-text').text(number);
-        //     $('#history-text').text(number);
-        //     inputArr.push(number)
-        //     console.log(inputArr)
-        //     //console.log(e);
-        //     printToDisplay();
+        // refactor for below .bind();
+        var number = $(this).attr('value');
+        $('#output-text').text(number);
+        $('#history-text').text(number);
+        inputArr.push(number)
+        console.log(inputArr)
+        //console.log(e);
+        printToDisplay();
     }
 
     function compute() {
         test = inputArr.join('');
         equals = math.eval(test);
+        var ans = math.format(equals, {precision: 14}); //to prevent rounding errors
         console.log('test', test, 'equals', equals)
-        console.log(typeof test);
-        $('#output-text').text(equals);
+        //console.log(typeof test);
+        // $('#output-text').text(equals);
+        $('#output-text').text(ans);
         historyArr.push(test);
         $('#history-text').text(test);
         inputArr = [];
-        inputArr.push(equals);
+        inputArr.push(ans);
         equality = true;
-        console.log('equality', equality);
+        
+        console.log('Equal sign equality: ', equality);
+        console.log('history-text', historyArr);
+    };
+
+    function operatorCompute() {
+        test = inputArr.join('');
+        equals = math.eval(test);
+        console.log('test', test, 'equals', equals)
+        //console.log(typeof test);
+        $('#output-text').text(equals);
+        historyArr.push(test);
+        $('#history-text').text(test);
+        
+        inputArr.push(equals);
+        
+        console.log('Equal sign equality: ', equality);
         console.log('history-text', historyArr);
     };
 
@@ -74,15 +97,16 @@ $(document).ready(function () {
     $('#clear-entry').click(function () {
         inputArr = [];
         $('#output-text').text(clearArr[0]);
+        decimalReset();
         console.log('Clear Entry', inputArr);
     });
 
-    // number button click event
-    $('.number').click(function (e) {
-        //errantZero();
-        if (equality) {
-            console.log(equality);
-            initialize();
+// Decimal Button Click event
+    $('#decimal').click(function (e) {
+        if (decimal && equality) {
+            equalityReset();
+            inputArr = [];
+
             var number = $(this).attr('value');
             $('#output-text').text(number);
             $('#history-text').text(number);
@@ -90,8 +114,12 @@ $(document).ready(function () {
             console.log(inputArr)
             //console.log(e);
             printToDisplay();
+            decimal = true;
+            console.log('decimal: ', decimal);
+        } else if (decimal && !equality) {
+            console.log('decimal should be true: ', decimal);
+            e.preventDefault();
         } else {
-            console.log(equality);
             var number = $(this).attr('value');
             $('#output-text').text(number);
             $('#history-text').text(number);
@@ -99,15 +127,19 @@ $(document).ready(function () {
             console.log(inputArr)
             //console.log(e);
             printToDisplay();
-        }
+            decimal = true;
+            console.log('decimal: ', decimal);
+        };
     });
 
-    // operator button click
-    $('.operator').click(function (e) {
+// Number button click event
+    $('.number').click(function (e) {
         if (equality) {
-            //insert solution here!!!
-            equality = false;
-            console.log(equality);
+            console.log('Number equality check 1: ', equality);  
+            equalityReset();
+            console.log('Number equality check 2: ', equality);  
+            //initialize();
+            inputArr = [];
             
             var number = $(this).attr('value');
             $('#output-text').text(number);
@@ -117,7 +149,8 @@ $(document).ready(function () {
             //console.log(e);
             printToDisplay();
         } else {
-            console.log(equality);
+            console.log('Number equality check:', equality);
+            //numberInput();
             var number = $(this).attr('value');
             $('#output-text').text(number);
             $('#history-text').text(number);
@@ -128,6 +161,37 @@ $(document).ready(function () {
         }
     });
 
+// Operator button click
+    $('.operator').click(function (e) {
+        if (equality) {
+            
+            console.log('Operator Eqauality Check 1: ', equality);
+            equalsReset();
+            decimalReset();
+            equalityReset();
+            console.log('Operator Eqauality Check 2: ', equality);
+
+            var number = $(this).attr('value');
+            $('#output-text').text(number);
+            $('#history-text').text(number);
+            inputArr.push(number)
+            console.log(inputArr)
+            //console.log(e);
+            printToDisplay();
+        } else {
+            decimalReset();
+            console.log('Operator Eqauality Check: ', equality);
+            var number = $(this).attr('value');
+            $('#output-text').text(number);
+            $('#history-text').text(number);
+            inputArr.push(number)
+            console.log(inputArr)
+            //console.log(e);
+            printToDisplay();
+        }
+    });
+
+//Equal Sign Click listener
     $('#equals').click(function () {
         compute();
     });
