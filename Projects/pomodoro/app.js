2a0a751db848e0f63f1b35982106f4f23e80629c
Christian Zenaty <cazenaty@yahoo.com>
2017-06-26 18:01:14 -0700

    fixed jquery issue

--- Projects/pomodoro/app.js
+++ Projects/pomodoro/app.js
@@ -1,94 +1,100 @@
-//Fix doc.ready
 //convert function
 //reset ()
 
-//$(document).ready(function () {
+$(document).ready(function () {
 
-// 1 min = 60,000 ms;
-// 1 sec = 1000ms;
-// 5 min = 300 sec;
+    // 1 min = 60,000 ms;
+    // 1 sec = 1000ms;
+    // 5 min = 300 sec;
 
-var time = 98;
-var nIntervalId;
-var min;
-var sec;
-var totalSec;
+    var time = 25;
+    var nIntervalId;
+    var min;
+    var sec;
+    var totalSec;
 
-var $minutes = $('.minutes');
-var $seconds = $('.seconds');
+    var $minutes = $('.minutes');
+    var $seconds = $('.seconds');
 
-function init() {
-    convertTime();
-    renderHTML();
-};
-init();
+    function init() {
+        convertTime();
+        renderHTML();
+    };
+    init();
 
-function renderHTML() {
-    $minutes.text((min < 10 ? ('0' + min) : min));
-    $seconds.text((sec < 10 ? ('0' + sec) : sec))
-}
-
-function convertTime() {
-    totalSec = time * 60;
-    min = Math.floor(totalSec / 60);
-    sec = totalSec % 60;
-    return totalSec, min, sec;
-}
-
-function startTimer() {
-    nIntervalId = setInterval(countdown, 1000);
-    console.log('StartTimer');
-};
-
-function countdown() {
-    if (totalSec <= 0) {
-        stopTimer();
-    } else {
-        totalSec -= 1;
-        console.log('Countdown - Minutes: ', min, 'Seconds: ', totalSec);
-        updateTime(totalSec);
+    function renderHTML() {
+        $minutes.text((min < 10 ? ('0' + min) : min));
+        $seconds.text((sec < 10 ? ('0' + sec) : sec))
     }
-};
 
-function stopTimer() {
-    clearInterval(nIntervalId);
-    console.log('Time Done, stopTimer() executed');
-};
-
-function updateTime(x) {
-    // convertTime();
-    min = Math.floor(x / 60);
-    sec = (x % 60);
-    console.log('renderHTML: min:', min, 'sec: ', sec);
-    renderHTML()
-};
-
-function addTime() {
-    return ++time;
-};
-
-function subtractTime() {
-    return --time;
-};
-
-$('.plus').click(function (e) {
-    if (time >= 100) {
-        e.preventDefault()
-    } else {
-        addTime();
-        init();
-        console.log(time, totalSec);
+    function convertTime() {
+        totalSec = time * 60;
+        min = Math.floor(totalSec / 60);
+        sec = totalSec % 60;
+        return totalSec, min, sec;
     }
+
+    function startTimer() {
+        nIntervalId = setInterval(countdown, 1000);
+        console.log('StartTimer');
+    };
+
+    function countdown() {
+        if (totalSec <= 0) {
+            stopTimer();
+        } else {
+            totalSec -= 1;
+            console.log('Countdown - Minutes: ', min, 'Seconds: ', totalSec);
+            updateTime(totalSec);
+        }
+    };
+
+    function stopTimer() {
+        clearInterval(nIntervalId);
+        console.log('Time Done, stopTimer() executed');
+    };
+
+    function updateTime(x) {
+        // convertTime();
+        min = Math.floor(x / 60);
+        sec = (x % 60);
+        console.log('renderHTML: min:', min, 'sec: ', sec);
+        renderHTML()
+    };
+
+    function addTime() {
+        return ++time;
+    };
+
+    function subtractTime() {
+        return --time;
+    };
+
+    $('.start').click(function () {
+        startTimer()
+    });
+    $('.stop').click(function () {
+        stopTimer()
+    });
+
+    $('.plus').click(function (e) {
+        if (time >= 100) {
+            e.preventDefault()
+        } else {
+            addTime();
+            init();
+            console.log(time, totalSec);
+        }
+    });
+
+    $('.minus').click(function (e) {
+        if (time <= 1) {
+            e.preventDefault()
+        } else {
+            subtractTime();
+            init();
+            console.log(time, totalSec);
+        }
+    });
+
 });
-
-$('.minus').click(function (e) {
-    if (time <= 1) {
-        e.preventDefault()
-    } else {
-        subtractTime();
-        init();
-        console.log(time, totalSec);
-    }
-});
-
-//});
