05cdc53321d6e3908fcbd20d877cfd3bbd0fef30
Christian Zenaty <cazenaty@yahoo.com>
2017-06-27 22:16:24 -0700

    updated layout

---
+++ Projects/tic_tac_toe/app.js
@@ -0,0 +1,7 @@
+// player turn determined by even of odd move (%2)
+// modal to choose X or O's
+// modal to start game
+
+// [ ] User Story: I can play a game of Tic Tac Toe with the computer.
+// [ ] User Story: My game will reset as soon as it's over so I can play again.
+// [ ] User Story: I can choose whether I want to play as X or O.
---
+++ Projects/tic_tac_toe/readme.md
@@ -0,0 +1,7 @@
+# TIC TAC TOE for FCC Front-End Projects
+
+## User Stories
+
+[ ] I can play a game of Tic Tac Toe with the computer.
+[ ] My game will reset as soon as it's over so I can play again.
+[ ] I can choose whether I want to play as X or O.
