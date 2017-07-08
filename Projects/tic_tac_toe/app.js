e3523453a77873f50130de3eb5bf57f48100b30b
Christian Zenaty <cazenaty@yahoo.com>
2017-07-07 20:04:56 -0700

    fixed double marking in gameplay

--- Projects/tic_tac_toe/app.js
+++ Projects/tic_tac_toe/app.js
@@ -2,6 +2,52 @@
 // modal to choose X or O's
 // modal to start game
 
+//X's turn on odd number moves, O's on even
+
 // [ ] User Story: I can play a game of Tic Tac Toe with the computer.
 // [ ] User Story: My game will reset as soon as it's over so I can play again.
 // [ ] User Story: I can choose whether I want to play as X or O.
+
+$(document).ready(function () {
+    const playerX = 'X';
+    const playerO = 'O';
+    const $square = $('.square');
+
+    var turnCount = 1;
+
+    function resetGame() {
+        turnCount = 1;
+        console.log('turnCount: ', turnCount, '\nX\'s turn');
+    };
+
+    function turn() {
+        if (turnCount === 9) {
+            console.log('game over');
+            resetGame();
+        } else if (turnCount % 2 === 1) {
+            console.log('X has gone, O\'s turn');
+            ++turnCount;
+            console.log('Turn:', turnCount, '\n ------------');
+        } else {
+            console.log('O has gone, X\'s turn');
+            ++turnCount;
+            console.log('Turn:', turnCount, '\n ------------');
+        }
+    };
+
+    $square.click(function (e) {
+        if (turnCount % 2 === 1 && e.target.textContent !== 'O') {
+            $(this).text('X');
+            console.log($(this));
+            console.log('E: ', e)
+            turn();
+        } else if (turnCount % 2 === 0 && e.target.textContent !== 'X') {
+            $(this).text('O');
+            turn();
+        } else {
+            console.log('Cannot Move here!');
+        }
+    });
+
+    resetGame();
+});
